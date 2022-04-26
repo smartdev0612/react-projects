7710bc53f8f3e54a693444848d0158bec4ad31a7
daniel0612 <goodluck1031@outlook.com>
2022-04-26 10:18:25 -0700

    update cocktail project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolor et quibusdam molestiae voluptatum at ratione officiis ducimus deleniti voluptatibus delectus magnam! Repellendus rem aperiam distinctio velit est praesentium voluptatibus.</p>
+    </section>
   )
 }
 
